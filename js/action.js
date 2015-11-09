019ddfb7c8146e2c99e13471c17f0d4d08397f13
barbaromatrix <matheus.galdino@hotmail.com>
2015-11-09 21:14:58 -0200

    Mudanças background-image

--- js/action.js
+++ js/action.js
@@ -1,19 +1,24 @@
 //Variável para controle de menu;
-var flag_menu;
+var click_menu;
 
 $(document).ready(function () {
-	flag_menu = false;
+	click_menu = false;
 		//verifica  se o width é menor que 767
 	if ($(document).width() < 767) {
-		alert($(document).width());
+
 		$('#button_menu').click(function (){
-			if (flag_menu == false) {
-				flag_menu = true;
+
+			if (click_menu == false) {
+
+				click_menu = true;
 				$('#jumbotron_home').css({"margin-top": "-50px"});
-			} else{
-				flag_menu = false;
-				$('#jumbotron_home').css({"margin-top": "100px"});
+			}
+			else {
+				
+				click_menu = false;
+				$('#jumbotron_home').css({"margin-top": "200px"});
 			};
+
 		});
 	};
 });
